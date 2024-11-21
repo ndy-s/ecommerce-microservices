dfbe5278fbf0cd8384d7fa67190cff95997e8eb5
ndys <hendy.sg02@gmail.com>
2024-11-22 01:09:15 +0700

    [fix] AppServiceProdvider: remove unnecessary import

--- order-service/app/Providers/AppServiceProvider.php
+++ order-service/app/Providers/AppServiceProvider.php
@@ -2,10 +2,7 @@
 
 namespace App\Providers;
 
-use App\Events\OrderCreated;
-use App\Listeners\ProcessOrder;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
